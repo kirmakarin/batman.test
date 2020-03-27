b9fb266849b799791234113de973314877838ba3
Kirill Makarin <36709321+kirmakarin@users.noreply.github.com>
2020-03-27 23:17:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    let tabsItem = $('.tabs-item');
+    
+    tabsItem.on('click', function(event){
+        event.preventDefault();
+        let activeContent = $(this).attr('href');
+        $('.visible').toggleClass('visible');
+        $('.tabs-selected').toggleClass('tabs-selected');
+        $(activeContent).toggleClass('visible');
+        $(this).toggleClass('tabs-selected');
+    });
+});
